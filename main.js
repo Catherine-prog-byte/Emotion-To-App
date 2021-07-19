aa2bec8c6ae13221edafe1f54f498a4fc3a48095
Catherine-prog-byte <77283675+Catherine-prog-byte@users.noreply.github.com>
2021-07-19 19:32:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,29 @@
+prediction_1="";
+prediction_2="";
 Webcam.set({
-   width : 350,
-   height : 350,
-   image_format : 'png',
-  png_quality : 90  
-});
-camera=document.getElementById("camera");
-Webcam.attach('#camera');
-function take_snapshot(){
-    Webcam.snap(function (data_uri)
-    {
-        document.getElementById("result").innerHTML="<img id='captured_image' src='"+ data_uri+"'/>";
-     });
-}
-console.log("ml5 version:", ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+    width : 350,
+    height : 350,
+    image_format : 'png',
+   png_quality : 90  
+ });
+ camera=document.getElementById("camera");
+ Webcam.attach('#camera');
+ function takeSnapshot(){
+     Webcam.snap(function (data_uri)
+     {
+         document.getElementById("result").innerHTML="<img id='captured_image' src='"+ data_uri+"'/>";
+      });
+    speak();
+ }
+ console.log("ml5 version:", ml5.version);
+ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/2I8BXL_SR/model.json',modelLoaded);
 function modelLoaded() {
     console.log("model loaded");
 }
-function check()  {
-      img=document.getElementById("captured_image");
-       classifier.classify(img,gotResult);
-}
-function gotResult(error , results) {
-     if (error) {
-         console.log(error);
-     }  
-     else {
-         console.log(results);
-         document.getElementById("result_object_name").innerHTML=results[0].label;
-         document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
-     }  
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data_1="The First Prediction Is "+prediction_1;
+    speak_data_2="And The Second Prediction Is "+prediction_2;
+    var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    synth.speak(utterThis);
 }
